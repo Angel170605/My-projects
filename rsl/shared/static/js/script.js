bcebed4a952b076fbb1a1f987f70723a9f298288
Ángel D <angeld@MacBook-Air-de-Angel.local>
2024-12-14 10:30:06 +0000

    Se corrige fallo en el cambio de tema

--- rsl/shared/static/js/script.js
+++ rsl/shared/static/js/script.js
@@ -1,11 +1,13 @@
 const darkMode = () => {
   document.querySelector("body").setAttribute("data-bs-theme", "dark");
   document.querySelector("#d1-icon").setAttribute("class", "bi bi-sun-fill");
+  localStorage.setItem("theme", "dark");
 };
 
 const lightMode = () => {
   document.querySelector("body").setAttribute("data-bs-theme", "light");
   document.querySelector("#d1-icon").setAttribute("class", "bi bi-moon-fill");
+  localStorage.setItem("theme", "light");
 };
 
 const changeTheme = () => {
@@ -13,3 +15,14 @@ const changeTheme = () => {
     ? darkMode()
     : lightMode();
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    if (savedTheme === "dark") {
+      darkMode();
+    } else {
+      lightMode();
+    }
+  }
+});

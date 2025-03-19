bc382bd851ced4f8b70669ea0522f988764b9a06
Angel <145053589+Angel170605@users.noreply.github.com>
2025-03-19 12:39:43 +0000

    Delete rsl directory

--- rsl/players/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Player
-
-
-@admin.register(Player)
-class PlayerAdmin(admin.ModelAdmin):
-    list_display = ['user', 'birthdate', 'country', 'number', 'position', 'played', 'goals', 'assists', 'yellow_cards', 'red_cards']
